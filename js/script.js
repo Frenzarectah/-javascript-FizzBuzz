fcb7ccefaaa90fdc032e592645c961217ffa2690
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-19 20:17:45 +0200

    -adding documentation to the script

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ appending = (thing) =>{                                         //la funzione ap
     root[0].appendChild(span);                                  //soprattutto all'interno del ciclo for
 }
 fizzbuzz = () =>{
-    var btn = document.getElementById("start");                 
-    var thing;
-    btn.remove();
+    var btn = document.getElementById("start");                 // btn fa riferimento all'id start (quindi il bottone di avvio)
+    var thing;                                                  //utile sia per avviare il gioco che per eliminarlo dal dom 
+    btn.remove();                                               //una volta iniziata l'esecuzione
     var body = document.getElementsByTagName("body");
     body[0].classList.add("listing");
         for(i=1;i<=100;i++){
-            if ((i%3==0)&&(i%5==0)) thing ="fizzbuzz";
-            else if (i%3==0) thing = "fizz";
-            else if (i%5==0) thing ="buzz";
-            else thing = i;
-            appending(thing);
-        }
+            if ((i%3==0)&&(i%5==0)) thing ="fizzbuzz";          //un for che va da 0 a 100, l'indice fa da riferimento anche per
+            else if (i%3==0) thing = "fizz";                    //elaborare correttamente il risultato, se l'indice (i) è divisibile
+            else if (i%5==0) thing ="buzz";                     //per 3, la stringa da dare in pasto alla funzione appending sarà fizz
+            else thing = i;                                     //per i multipli di 5 sarà buzz e i multipli di entrambi fizzbuzz
+            appending(thing);                                   //altrimenti il gioco prevede la stampa del numero in se, quindi semplicemente
+        }                                                       //il valore di i in quel momento.
 }
 
