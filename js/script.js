733e0484b8ecc11807b392124a0f2e71ce92d9ae
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-19 20:00:49 +0200

    finishing project -adjusting UX with flex:wrap, review of script and commenting -adding another background for the mobile

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
+appending = (thing) =>{                                         //la funzione appending si occupa di costruire l'elemento
+    root = document.getElementsByClassName("container");        //span in base alla variabile thing, che conterrà la stringa
+    var span = document.createElement("span");                  //elaborata dagli if presenti nella funzione principale fizzbuzz.
+    span.innerHTML = thing;                                     //In questo modo si rende piu' snello e capibile il codice,
+    root[0].appendChild(span);                                  //soprattutto all'interno del ciclo for
+}
 fizzbuzz = () =>{
-    var btn = document.getElementById("start");
+    var btn = document.getElementById("start");                 
     var thing;
     btn.remove();
     var body = document.getElementsByTagName("body");
     body[0].classList.add("listing");
-    for(i=1;i<=30;i++){
-        if ((i%3==0)&&(i%5==0)) thing ="fizzbuzz";
-        else if (i%3==0) thing = "fizz";
-        else if (i%5==0) thing ="buzz";
-        else thing = i;
-        appending(thing);
-    }
+        for(i=1;i<=100;i++){
+            if ((i%3==0)&&(i%5==0)) thing ="fizzbuzz";
+            else if (i%3==0) thing = "fizz";
+            else if (i%5==0) thing ="buzz";
+            else thing = i;
+            appending(thing);
+        }
+}
 
-}
-appending = (thing) =>{
-    root = document.getElementsByClassName("container");
-    var span = document.createElement("span");
-    span.classList.add("textOrbitron","textOrbitron--25");
-    span.innerHTML = thing;
-    root[0].appendChild(span);
-}
