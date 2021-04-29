bd3d32d159f4683ae2f8b92792739a983ea89f15
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-04-29 17:30:09 +0200

    adding some other tests on test file

--- js/for.test.js
+++ js/for.test.js
@@ -1,5 +1,16 @@
 const sum = require('./script');
+const notnull = require('./script');
+const is_NaN = require('./script');
 
-test('going from 1 to 100', () => {
+test('somma prova', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+test('x will be not null', ()=> {
+  expect(notnull(1)).not.toBeNull();
+  expect(notnull("1")).not.toBeNull();
+});
+
+test('x have to be a number',()=>{
+  expect(is_NaN("ciao")).toBeTruthy();
+});
