497862329d5ab90ceabbc0ea1770736c5fe92cdd
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-05-03 19:39:24 +0200

    adding another test unit - provide a test for the "submit" function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-var appending = (thing) =>{                                         //la funzione appending si occupa di costruire l'elemento
-    root = document.getElementsByClassName("container");        //span in base alla variabile thing, che conterrà la stringa
-    var span = document.createElement("span");                  //elaborata dagli if presenti nella funzione principale fizzbuzz.
-    span.innerHTML = thing;                                     //In questo modo si rende piu' snello e capibile il codice,
+var appending = (thing) =>{                                         
+    root = document.getElementsByClassName("container");        
+    var span = document.createElement("span");                  
+    span.innerHTML = thing;                                     
     root[0].appendChild(span);                                  
 }
-var notnull = (x) =>{
-    if (x !== "null") return true;
-}
-var is_NaN = (x) =>{
-    if (isNaN(x))  return true;
-}
-var submit = () =>{
-    var passed = true;
-    var error ="Il form presenta i seguenti errori:\n";
+var extractForm = () =>{
     var result= [];
     result[0] = document.getElementsByClassName("min")[0].value;
     result[1] = document.getElementsByClassName("max")[0].value;
@@ -20,20 +12,9 @@ var submit = () =>{
     result[3] = document.getElementsByClassName("div2")[0].value;
     result[4] = document.getElementsByClassName("word1")[0].value;
     result[5] = document.getElementsByClassName("word2")[0].value;
-    console.log(result);
-    for(i=0;i<=result.length-1;i++){
-            console.log(result[i]);
-        if (empty(result[i]) === true){ 
-            passed = false; 
-            error += "il "+(i+1)+"° campo è vuoto\n";
-        }else if (isNaN(result[i])&&(i!==4)&&(i!==5)){
-            passed = false;
-            error +="il"+(i+1)+"° campo non è un numero\n";
-        }
-    }    
-        if (passed === true) fizzbuzz(result);
-        else alert(error);
-    }
+    return result;
+}
+
 
 var fizzbuzz = (result) =>{
     var win = document.getElementsByClassName("window")[0];
@@ -55,5 +36,26 @@ var empty = (x) =>{
     if ((x === null) || (x === undefined)|| (x ==="")) return true;
     else return false;
 }
+var submit = (result) =>{
+    var passed = true;
+    var error ="Il form presenta i seguenti errori:\n";
+    console.log(result);
+    for(i=0;i<=result.length-1;i++){
+        if (empty(result[i]) === true){ 
+            passed = false; 
+            error += "il "+(i+1)+"° campo è vuoto\n";
+        }else if (isNaN(result[i])&&(i!==4)&&(i!==5)){
+            passed = false;
+            error +="il"+(i+1)+"° campo non è un numero\n";
+        }
+    }    
+        if (passed === true){ 
+            fizzbuzz(result);
+            return passed;
+        } else{ 
+            alert(error);
+            return passed;
+        }
+    }
 
-module.exports = {empty};
+module.exports = {empty,submit};
--- js/test.unit.js
+++ js/test.unit.js
@@ -7,3 +7,8 @@ test('x will be not null', ()=> {
   expect(funct.empty(1)).not.toBeTruthy();
 });
 
+test('this function will return false if array incomplete', () => {
+  arrai = [1,20,"",4,"parola","parola2"];
+  expect(funct.submit(arrai)).toBeFalsy();
+});
+
